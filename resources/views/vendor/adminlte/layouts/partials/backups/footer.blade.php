5e5e2d41519729eec454d4bc191ce593bbbb71e8
Rodrigo <rimposti@gmail.com>
2016-12-03 21:48:30 -0300

    Pulida de Integracion con adminlte

--- resources/views/vendor/adminlte/layouts/partials/backups/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>dondeDuele</b></a>
-    </div>
-    <strong>Copyright &copy; 2016</strong>
-</footer>
