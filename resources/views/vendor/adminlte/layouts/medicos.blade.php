2f02e603e42712ac6f061eaacea5409c6905f22c
martindiiorio <MartinDiIorio@md.local>
2016-12-06 23:45:13 -0300

    fix doctors view

--- resources/views/vendor/adminlte/layouts/medicos.blade.php
+++ resources/views/vendor/adminlte/layouts/medicos.blade.php
@@ -75,12 +75,21 @@
               <div id="todos" class="tab-content current">
                 @if (count($users) > 0)
                     @foreach ($users as $user)
-                      <a href="{{ url('/medico') . '/' . $user->id }}" class="search_results">
-                        <img class="avatar" src="{{ url('/storage') . '/' . $user->avatar }}" alt="" >
-                        <h3>{{ $user->name }}</h3>
-                        <p>{{ $user->especialidad }}</p>
-                        <p>{{ $user->direccion }}</p>
-                      </a>
+                      <article class="row todosMedicos">
+                        <div class="col-sm-2"></div>
+                        <div class="col-sm-2 todosMedicos-1">
+                          <img class="avatar" src="{{ url('/storage') . '/' . $user->avatar }}" alt="" >
+                        </div>
+                        <div class="col-sm-5 todosMedicos-2">
+                          <a href="{{ url('/medico') . '/' . $user->id }}" class="">{{ $user->name }}</a>
+                          <p>{{ $user->especialidad }}</p>
+                          <p><i class="fa fa-map-marker" aria-hidden="true"></i>{{ $user->direccion }}</p>
+                        </div>
+                        <div class="col-sm-3 todosMedicos-3">
+                          <a><i class="fa fa-phone" aria-hidden="true"></i>Ver teléfono</a>
+                          <a><i class="fa fa-envelope-o" aria-hidden="true"></i>Ver mail</a>
+                        </div>
+                      </article>
                     @endforeach
                 @else
                    <p>No se encontraron resultados</p>
@@ -91,8 +100,22 @@
               <div id="medicos" class="tab-content">
                 @if (count($users) > 0)
                     @foreach ($users as $user)
-                      <article><a href="{{ url('/medico') . '/' . $user->id }}">{{ $user->name }}</a></article>
-                    @endforeach
+                    <article class="row todosMedicos">
+                      <div class="col-sm-2"></div>
+                      <div class="col-sm-2 todosMedicos-1">
+                        <img class="avatar" src="{{ url('/storage') . '/' . $user->avatar }}" alt="" >
+                      </div>
+                      <div class="col-sm-5 todosMedicos-2">
+                        <a href="{{ url('/medico') . '/' . $user->id }}" class="">{{ $user->name }}</a>
+                        <p>{{ $user->especialidad }}</p>
+                        <p><i class="fa fa-map-marker" aria-hidden="true"></i>{{ $user->direccion }}</p>
+                      </div>
+                      <div class="col-sm-3 todosMedicos-3">
+                        <a><i class="fa fa-phone" aria-hidden="true"></i>Ver teléfono</a>
+                        <a><i class="fa fa-envelope-o" aria-hidden="true"></i>Ver mail</a>
+                      </div>
+                    </article>
+                  @endforeach
                 @else
                    <p>No se encontraron resultados</p>
                 @endif
